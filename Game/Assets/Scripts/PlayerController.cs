4c45ac85032c2c850e2d047f04d3c7642879ea25
Prescott Lawani <38223423+Lockeheede@users.noreply.github.com>
2025-01-30 18:12:37 -0600

    laptop update
    -->Added logic for collecting items through trigger, User Interface, making objects dynamic for optimization, and a win condition.

--- Game/Assets/Scripts/PlayerController.cs
+++ Game/Assets/Scripts/PlayerController.cs
@@ -1,17 +1,25 @@
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem; // Required for InputSystem
+using TMPro; // Required for TextMeshPro
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 0; // Speed variable that can be set in the Unity Editor
+    public TextMeshProUGUI countText; // Reference to the TextMeshProUGUI component
+    public GameObject winTextObject; // Reference to the GameObject component
     private Rigidbody rb; // Reference to the Rigidbody component
+    private int count;
     private float movementX;
     private float movementY;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>(); // Get the Rigidbody component and set it to the rb variable.
+        count = 0; // Set the count variable to 0
+
+        SetCountText(); // Call the SetCountText function
+        winTextObject.SetActive(false); // Set the winTextObject to inactive
     }
 
     void OnMove(InputValue movementValue)
@@ -21,11 +29,29 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y; // Set the movementY variable to the y value of the movement vector
     }
 
+    void SetCountText() {
+        countText.text = "Count: " + count.ToString(); // Set the text of the countText to "Count: " + the value of the count variable
+        if(count >= 12)
+        {
+            winTextObject.SetActive(true); // Set the winTextObject to active
+        }
+    }
+
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY); // Create a new Vector3 with the movement values
 
         rb.AddForce(movement * speed); // Add a force to the Rigidbody component
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp")) // Check if the object that the player collided with has the tag "PickUp"
+        {
+            other.gameObject.SetActive(false); // Set the object that the player collided with to inactive
+            count++; // Increment the count variable
+            SetCountText(); // Call the SetCountText function
+        }
     }
 
 }
